d96b1eb1ff6b00653152321661ba72fc4ecc2953
Vadim <vadim5363@yandex.ru>
2023-04-10 20:50:47 +0300

    Сверстал страницу для разрешения 360px, убрал скролл при открытии меню

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 
 const menuIcon = document.querySelector(".menu__icon");
 const menu = document.querySelector(".menu");
+const body = document.getElementsByTagName("body");
 console.log(menu, menuIcon);
 
 menuIcon.addEventListener("click", function () {
     menu.classList.toggle("_active");
     menuIcon.classList.toggle("_active");
+    body[0].classList.toggle("_active");
 });
